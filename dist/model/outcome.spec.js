074f7cf5c581347965bd8a4e01efe6b4e27b12bc
Matt West <matt.west@bjss.com>
2018-06-11 11:02:38 +0100

    More values to the Outcome object.

--- dist/model/outcome.spec.js
+++ dist/model/outcome.spec.js
@@ -6,9 +6,22 @@ describe("Outcome", () => {
     // tslint:disable-next-line:max-line-length
     const csv1 = ",16N023744,08A,999 123 4567,TEST,BABY,17/06/2016,2,,G83067,1,1,2518,36,1,,,TEST# Firstname,Flat 11# Test House,Test Quay,Woolwich,LONDON,London,SE18 5NH,,23/05/2016,22/05/2016,,SAMPLE TAKER,SEThames,4,,PKU Not Suspected. Status Code 04,4,,CHT Not Suspected. Status Code 04,6,602,Carrier of Other Haemoglobin. Status Code 06,4,,CF Not Suspected. Status Code 04,4,,MCADD Not Suspected. Status Code 04,4,,HCU Not Suspected. Status Code 04,4,,MSUD Not Suspected. Status Code 04,4,,GA1 Not Suspected. Status Code 04,4,,IVA Not Suspected. Status Code 04";
     describe("constructor", () => {
+        it("should throw Error if there are not the correct number of values", () => {
+            expect(() => {
+                subject = new outcome_1.Outcome("rubbish, values");
+            }).toThrow(new Error("Invalid values: string has 2 values but was expecting 57."));
+        });
         it("should set the fields", () => {
             subject = new outcome_1.Outcome(csv1);
             expect(subject.labSerialNo).toBe("16N023744");
+            expect(subject.nationalId).toBe("");
+            expect(subject.providerUnit).toBe("08A");
+            expect(subject.nhsNo).toBe("9991234567");
+            expect(subject.surname).toBe("TEST");
+            expect(subject.labCode).toBe("SEThames");
+            expect(subject.pkuStatusCode).toBe("4");
+            expect(subject.pkuSupplementaryCode).toBe("");
+            expect(subject.pkuStatus).toBe("PKU Not Suspected. Status Code 04");
         });
     });
 });
--- src/model/outcome.ts
+++ src/model/outcome.ts
@@ -5,6 +5,8 @@
  */
 export class Outcome {
 
+    private static MaxFields = 57;
+
     public nationalId = "";
     public labSerialNo = "";
     public providerUnit = "";
@@ -18,10 +20,29 @@ export class Outcome {
 
     // private dateOfBirth: Date;
 
+    /**
+     * Constructs an Outcome object.
+     * @param allValues a comma-separated list of values.
+     * @throws Error if the values are invalid.
+     */
     constructor(public allValues: string) {
         const fields = allValues.split(",");
+
+        if (fields.length !== Outcome.MaxFields) {
+            throw new Error("Invalid values: string has " + fields.length
+              + " values but was expecting " + Outcome.MaxFields + ".");
+        }
+
         // this.dateOfBirth = new Date();
         this.nationalId = fields[0];
         this.labSerialNo = fields[1];
+        this.providerUnit = fields[2];
+        this.nhsNo = fields[3].replace(/ /g, "");  // "NNN NNN NNN" is a display string for "NNNNNNNNN"
+        this.surname = fields[4];
+
+        this.labCode = fields[29];
+        this.pkuStatusCode = fields[30];
+        this.pkuSupplementaryCode = fields[31];
+        this.pkuStatus = fields[32];
     }
 }
