f719a797da75e83c416851f62c12d2334580d502
BJSS\gopal.thirumurthy <gopal.thirumurthy@bjss.com>
2018-07-06 16:07:57 +0100

    Added cases for status code error for all procedures. Added test to verify tool reports error in the case of nhs number length is longer than 10 digits.

--- test/testGroup/error_cases.js
+++ test/testGroup/error_cases.js
@@ -32,15 +32,26 @@ describe('  ***** Verifying Error cases *****', function() {
         expect(stdOut).toContain(error_response);
     });
 
-    it('should error if PKU status code is not in the range of 1 to 10', async function(){
-        var error_file = srcFiles.filter((value) => {return value.includes("error_PKU_Status_Code");})[0];
-        var error_response = "PKU status code should be a number between 1 and 10";
+    it('should error all Procedures if status code is not in the range of 1 to 10', async function(){
+        var error_file = srcFiles.filter((value) => {return value.includes("error_Procedures_Status_Code");})[0];
+        var procedure_erros = [];
+        procedure_erros.push("line 2: Error: PKU status code should be a number between 1 and 10");
+        procedure_erros.push("line 3: Error: CHT status code should be a number between 1 and 10");
+        procedure_erros.push("line 4: Error: SCD status code should be a number between 1 and 10");
+        procedure_erros.push("line 5: Error: CF status code should be a number between 1 and 10");
+        procedure_erros.push("line 6: Error: MCADD status code should be a number between 1 and 10");
+        procedure_erros.push("line 7: Error: HCU status code should be a number between 1 and 10");
+        procedure_erros.push("line 8: Error: MSUD status code should be a number between 1 and 10");
+        procedure_erros.push("line 10: Error: IVA status code should be a number between 1 and 10");
+        
         var outFolder = 'testOutput/' + error_file.split('.')[0].split('/')[2];
             //Remove if folder already exists
         fileHandler.rmFolders(outFolder);
         var res =  await executeCommand('node dist/index.js '+error_file+" "+outFolder);
         var stdOut = res.success ?  res.message : res.error;
-        expect(stdOut).toContain(error_response);
+        for(var error of procedure_erros){
+            expect(stdOut).toContain(error);
+        }
      });
 
      it('should error if date of birth is in wrong format', async function(){
@@ -65,6 +76,18 @@ describe('  ***** Verifying Error cases *****', function() {
         var stdOut = res.success ?  res.message : res.error;
         expect(stdOut).toContain(error_collection_date);
         expect(stdOut).toContain(error_receipt_date);
+     });
+
+     it('should error if NHS number length is longer than 10 digits', async function(){
+        var error_file = srcFiles.filter((value) => {return value.includes("error_nhs_no_length");})[0];
+        var NHSno_error_length = "NHS Number value \"99912345678\" exceeds the maximum length of 10";
+
+        var outFolder = 'testOutput/' + error_file.split('.')[0].split('/')[2];
+            //Remove if folder already exists
+        fileHandler.rmFolders(outFolder);
+        var res =  await executeCommand('node dist/index.js '+error_file+" "+outFolder);
+        var stdOut = res.success ?  res.message : res.error;
+        expect(stdOut).toContain(NHSno_error_length);
      });
 });
 
